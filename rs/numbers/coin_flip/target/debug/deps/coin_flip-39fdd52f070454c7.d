bf86a8bc03a1ba7cf80577467eadf5753771980a
Liam <iliamftw2013@gmail.com>
2024-05-03 21:44:58 -0300

    Add( coin flip )

---
+++ rs/numbers/coin_flip/target/debug/deps/coin_flip-39fdd52f070454c7.d
@@ -0,0 +1,5 @@
+C:\Users\liams\Dropbox\Coding\zerotohero\rs\numbers\coin_flip\target\debug\deps\libcoin_flip-39fdd52f070454c7.rmeta: src\main.rs
+
+C:\Users\liams\Dropbox\Coding\zerotohero\rs\numbers\coin_flip\target\debug\deps\coin_flip-39fdd52f070454c7.d: src\main.rs
+
+src\main.rs:
---
+++ rs/numbers/coin_flip/target/debug/deps/coin_flip-5331510a28113f01.d
@@ -0,0 +1,5 @@
+C:\Users\liams\Dropbox\Coding\zerotohero\rs\numbers\coin_flip\target\debug\deps\libcoin_flip-5331510a28113f01.rmeta: src\main.rs
+
+C:\Users\liams\Dropbox\Coding\zerotohero\rs\numbers\coin_flip\target\debug\deps\coin_flip-5331510a28113f01.d: src\main.rs
+
+src\main.rs:
